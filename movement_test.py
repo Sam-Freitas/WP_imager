0eb009ee8ac47d8f5d6bb91eb980a8628e12c486
00vsilbar <samfreitas@email.arizona.edu>
2023-10-23 16:29:31 -0700

    samll update making sure the self function is working
    basically just made the movement functon mostly work was tryong to get the fluor to run but broke machine somehow and now have to fix -- not a major update

--- movement_test.py
+++ movement_test.py
@@ -86,44 +86,44 @@ class CNCController:
     
     def move_XY_at_Z_travel(self, position, z_travel_height):
 
-        current_position = CNCController.get_current_position()
+        current_position = CNCController.get_current_position(self)
 
         if round(float(current_position['z_pos']),1) != float(z_travel_height):
             #### go to z travel height
             command = "g0 z" + str(z_travel_height) + " " + "\n"
-            response, out = CNCController.send_command(command)
+            response, out = CNCController.send_command(self,command)
         
         print('moving to XY')
         command = 'G0 ' + 'X' + str(position['x_pos']) + ' ' + 'Y' + str(position['y_pos']) 
-        response, out = CNCController.send_command(command)
+        response, out = CNCController.send_command(self,command)
         ##### move z
         print('moving to Z')
         command = 'G0 ' + 'Z' + str(position['z_pos']) 
-        response, out = CNCController.send_command(command)
+        response, out = CNCController.send_command(self,command)
 
-        return CNCController.get_current_position()
+        return CNCController.get_current_position(self)
     
     def move_XYZ(self, position):
 
         ##### move xyz
         print('moving to XYZ')
         command = 'G0 ' + 'X' + str(position['x_pos']) + ' ' + 'Y' + str(position['y_pos']) + ' ' + 'Z' + str(position['z_pos']) 
-        response, out = CNCController.send_command(command)
+        response, out = CNCController.send_command(self,command)
 
-        return CNCController.get_current_position()
+        return CNCController.get_current_position(self)
     
     def home_grbl(self):
         print("HOMING CNC")
         command = "$H"+ "\n"
-        response, out = CNCController.send_command(command)
+        response, out = CNCController.send_command(self,command)
     
     def set_up_grbl(self, home = True):
         # unlock 
         command = "$X"+ "\n"
-        response, out = CNCController.send_command(command)
+        response, out = CNCController.send_command(self,command)
 
         command = "?"+ "\n"
-        response, out = CNCController.send_command(command)
+        response, out = CNCController.send_command(self,command)
 
         if home:
             CNCController.home_grbl(self)
@@ -159,15 +159,17 @@ if __name__ == "__main__":
 
     # run setup test to make sure everything works or throw error
     s_todays_runs = settings.get_settings.update_todays_runs(s_todays_runs, overwrite=True)
-    controller.set_up_grbl(home = True)
 
     plate_index = []
-    plate_positions = []
-    for this_plate_index in s_plate_names_and_opts['plate_name']:
+    plate_index_fluor = []
+    for this_plate_index in s_plate_names_and_opts['plate_index']:
         this_plate_name = s_plate_names_and_opts['plate_name'][this_plate_index]
         if this_plate_name != 'NONE':
             plate_index.append(this_plate_index)
+            if s_plate_names_and_opts['fluorescence'][this_plate_index]:
+                plate_index_fluor.append(this_plate_index)
 
+    controller.set_up_grbl(home = True)
     # # # run lifespan imaging experiments
     for this_plate_index in plate_index:
         # get the experiment options
@@ -194,7 +196,7 @@ if __name__ == "__main__":
     controller.set_up_grbl(home = True)
 
     # # # run fluorescent imaging experiments
-    for this_plate_index in plate_index:
+    for this_plate_index in plate_index_fluor:
         # get the experiment options
         this_plate_parameters,this_plate_position = settings.get_settings.get_indexed_dict_parameters(s_plate_names_and_opts,s_plate_positions,this_plate_index)
         print(this_plate_parameters)
@@ -221,7 +223,7 @@ if __name__ == "__main__":
         controller.move_XYZ(position = calibration_coordinates)
         # run the calibration script 
         print('running Z calibration script -------------------------------------------------------------------------------------')
-        calibration_coordinates['z_pos'] = -90 # calibration_coordinates['z_pos'] -40 ### do something here
+        calibration_coordinates['z_pos'] = -50 # calibration_coordinates['z_pos'] -40 ### do something here
 
         # fluorescently image each of the terasaki wells (96)
         for well_index,this_terasaki_well_xy in enumerate(zip(s_terasaki_positions['x_relative_pos_mm'].values(),s_terasaki_positions['y_relative_pos_mm'].values())):
