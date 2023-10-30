2c90635687da8b6e996f9a22703afea2c5cb614b
Samuel "Sam" Freitas <samfreitas@email.arizona.edu>
2023-10-30 14:37:52 -0700

    Update four_corner_calibration.py

--- four_corner_calibration.py
+++ four_corner_calibration.py
@@ -8,6 +8,12 @@ import camera.camera_control
 
 from Run_yolo_model import run_yolo_model
 
+import tkinter
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import scipy
+
 def turn_everything_off_at_exit():
     lights.labjackU3_control.turn_off_everything()
     cv2.destroyAllWindows()
@@ -137,6 +143,9 @@ class CNCController:
 def jprint(input):
     print(json.dumps(input,indent=4))
     
+
+
+
 import atexit
 
 if __name__ == "__main__":
@@ -154,73 +163,136 @@ if __name__ == "__main__":
     s_camera_settings = settings.get_settings.get_basic_camera_settings()
     s_todays_runs = settings.get_settings.get_todays_runs()
 
-    settings.get_settings.check_grbl_port(s_machines['grbl'][0], run_as_testing = False)
-    controller = CNCController(port=s_machines['grbl'][0], baudrate=s_machines['grbl'][1])
-    response, s_grbl_settings = controller.send_command("$$"+ "\n")
-    s_grbl_settings_df,s_grbl_settings = settings.get_settings.convert_GRBL_settings(s_grbl_settings)
-    z_travel_height = s_machines['grbl'][2]
-
-    # run setup test to make sure everything works or throw error
-    s_todays_runs = settings.get_settings.update_todays_runs(s_todays_runs, overwrite=True)
-    d = lights.labjackU3_control.setup_labjack(verbose=True)    # test the blue and red lights
-    lights.labjackU3_control.blink_led(d)
-
+    testing = True
     # get all the experiments that are not defunct
     plate_index = [0,7,64,71]
+    grid = [9,8] # rows, cols
 
-    lights.labjackU3_control.turn_on_red(d)
-    controller.set_up_grbl(home = True)
-    # # # run lifespan imaging experiments
+    if not testing:
 
-    output = []
+        settings.get_settings.check_grbl_port(s_machines['grbl'][0], run_as_testing = False)
+        controller = CNCController(port=s_machines['grbl'][0], baudrate=s_machines['grbl'][1])
+        response, s_grbl_settings = controller.send_command("$$"+ "\n")
+        s_grbl_settings_df,s_grbl_settings = settings.get_settings.convert_GRBL_settings(s_grbl_settings)
+        z_travel_height = s_machines['grbl'][2]
 
-    for this_plate_index in plate_index:
-        # get the experiment options
-        this_plate_parameters,this_plate_position = settings.get_settings.get_indexed_dict_parameters(s_plate_names_and_opts,s_plate_positions,this_plate_index)
-        print(this_plate_parameters)
-        print(this_plate_position)
+        # run setup test to make sure everything works or throw error
+        s_todays_runs = settings.get_settings.update_todays_runs(s_todays_runs, overwrite=True)
+        d = lights.labjackU3_control.setup_labjack(verbose=True)    # test the blue and red lights
+        lights.labjackU3_control.blink_led(d)
 
-        # get the position of the experiment
-        # move above the plate
-        position = this_plate_position.copy()
-        position['x_pos'],position['y_pos'],position['z_pos'] = round(position['x_pos'],4), round(position['y_pos'],4), -90
-        controller.move_XY_at_Z_travel(position = position,
-                                       z_travel_height = z_travel_height)
-        
-        image_filename = camera.camera_control.simple_capture_data_single_image(s_camera_settings, plate_parameters=this_plate_parameters,output_dir=output_dir, image_file_format = 'jpg')
-        individual_well_locations,center_location = run_yolo_model(img_filename=image_filename, plot_results = True)
+        lights.labjackU3_control.turn_on_red(d)
+        controller.set_up_grbl(home = True)
+        # # # run lifespan imaging experiments
 
-        well_locations_delta = individual_well_locations[-1]-individual_well_locations[0]
-        pixels_per_mm = well_locations_delta/[69.7,42] 
-        center = [float(s_camera_settings['widefield'][1])/2,float(s_camera_settings['widefield'][2])/2]
-        center_delta = center-center_location
-        center_delta_in_mm = center_delta/pixels_per_mm
-        calibration_coordinates = dict()
-        calibration_coordinates['x_pos'] = center_delta_in_mm[0]
-        calibration_coordinates['y_pos'] = center_delta_in_mm[1]
-        calibration_coordinates['z_pos'] = s_terasaki_positions['calib_z_pos_mm'][0]
+        corner_points = []
 
-        measured_position = controller.get_current_position()
-        adjusted_position = measured_position.copy()
-        adjusted_position['x_pos'] = measured_position['x_pos'] + center_delta_in_mm[0]
-        adjusted_position['y_pos'] = measured_position['y_pos'] - center_delta_in_mm[1]
-        controller.move_XYZ(position = adjusted_position)
-        image_filename = camera.camera_control.simple_capture_data_single_image(s_camera_settings,plate_parameters=this_plate_parameters, output_dir=output_dir, image_file_format = 'jpg')
-        individual_well_locations,center_location = run_yolo_model(img_filename=image_filename, plot_results = True)
+        for this_plate_index in plate_index:
+            # get the experiment options
+            this_plate_parameters,this_plate_position = settings.get_settings.get_indexed_dict_parameters(s_plate_names_and_opts,s_plate_positions,this_plate_index)
+            print(this_plate_parameters)
+            print(this_plate_position)
+
+            # get the position of the experiment
+            # move above the plate
+            position = this_plate_position.copy()
+            position['x_pos'],position['y_pos'],position['z_pos'] = round(position['x_pos'],4), round(position['y_pos'],4), -90
+            controller.move_XY_at_Z_travel(position = position,
+                                        z_travel_height = z_travel_height)
+            
+            image_filename = camera.camera_control.simple_capture_data_single_image(s_camera_settings, plate_parameters=this_plate_parameters,output_dir=output_dir, image_file_format = 'jpg')
+            individual_well_locations,center_location = run_yolo_model(img_filename=image_filename, plot_results = True)
+
+            well_locations_delta = individual_well_locations[-1]-individual_well_locations[0]
+            pixels_per_mm = well_locations_delta/[69.7,42] 
+            center = [float(s_camera_settings['widefield'][1])/2,float(s_camera_settings['widefield'][2])/2]
+            center_delta = center-center_location
+            center_delta_in_mm = center_delta/pixels_per_mm
+            calibration_coordinates = dict()
+            calibration_coordinates['x_pos'] = center_delta_in_mm[0]
+            calibration_coordinates['y_pos'] = center_delta_in_mm[1]
+            calibration_coordinates['z_pos'] = s_terasaki_positions['calib_z_pos_mm'][0]
+
+            measured_position = controller.get_current_position()
+            adjusted_position = measured_position.copy()
+            adjusted_position['x_pos'] = measured_position['x_pos'] + center_delta_in_mm[0]
+            adjusted_position['y_pos'] = measured_position['y_pos'] - center_delta_in_mm[1]
+            controller.move_XYZ(position = adjusted_position)
+            image_filename = camera.camera_control.simple_capture_data_single_image(s_camera_settings,plate_parameters=this_plate_parameters, output_dir=output_dir, image_file_format = 'jpg')
+            individual_well_locations,center_location = run_yolo_model(img_filename=image_filename, plot_results = True)
 
 
-        new_position = controller.get_current_position()
-        new_position['x_pos'],new_position['y_pos'],new_position['z_pos'] = round(new_position['x_pos'],4), round(new_position['y_pos'],4), -100
-        controller.move_XYZ(position = new_position)
-        image_filename = camera.camera_control.simple_capture_data_single_image(s_camera_settings,plate_parameters=this_plate_parameters, output_dir=output_dir, image_file_format = 'jpg')
-        individual_well_locations,center_location = run_yolo_model(img_filename=image_filename, plot_results = True, i = this_plate_index)
+            new_position = controller.get_current_position()
+            new_position['x_pos'],new_position['y_pos'],new_position['z_pos'] = round(new_position['x_pos'],4), round(new_position['y_pos'],4), -100
+            controller.move_XYZ(position = new_position)
+            image_filename = camera.camera_control.simple_capture_data_single_image(s_camera_settings,plate_parameters=this_plate_parameters, output_dir=output_dir, image_file_format = 'jpg')
+            individual_well_locations,center_location = run_yolo_model(img_filename=image_filename, plot_results = True, i = this_plate_index)
 
-        print(new_position)
-        output.append(new_position)
+            print(new_position)
+            corner_points.append(new_position)
 
-        time.sleep(0.05)
-        print('')
+            time.sleep(0.05)
+            print('')
 
-    for i,out in enumerate(output):
-        plate_index[i]
-        print(i,out)
+        for i,out in enumerate(corner_points):
+            plate_index[i]
+            print(i,out)
+    else:
+
+        corner_points = np.asarray([[-42.668,-111.716,-100],
+                            [-1111.444,-121.817,-100],
+                            [-42.265,-1029.433,-100],
+                            [-1110.465,-1038.979,-100]])
+
+    # dx = np.mean([corner_points[0,0] - corner_points[2,0], 
+    #                 corner_points[1,0] - corner_points[3,0]])
+    # dy = np.mean([corner_points[0,1] - corner_points[1,1], 
+    #             corner_points[2,1] - corner_points[3,1]])
+    # print('dx:', dx, 'dy:', dy) ########grid = [9,8] # rows, cols
+
+    # x = [corner_points[0,0],corner_points[-1,0]]
+    # y = [corner_points[0,1],corner_points[-1,1]]
+
+    # x = np.linspace(x[0],x[1],grid[1])
+    # y = np.linspace(y[0],y[1],grid[0])
+
+    # xv, yv = np.zeros(size = (9,8)), np.zeros(size = (9,8))
+
+    out = []
+
+    starting_x = np.linspace(corner_points[0,0],corner_points[2,0],9)
+    ending_x = np.linspace(corner_points[1,0],corner_points[3,0],9)
+
+    starting_y = np.linspace(corner_points[0,1],corner_points[2,1],9)
+    ending_y = np.linspace(corner_points[1,1],corner_points[3,1],9)
+
+    counter = 0
+    plt.figure(1)
+    for i in range(grid[0]): #rows
+
+        this_staring_x = starting_x[i]
+        this_staring_y = starting_y[i]
+
+        this_ending_x = ending_x[i]
+        this_ending_y = ending_y[i]
+
+        print(this_staring_x,this_staring_y, ' ----- to ----- ',this_ending_x,this_ending_y)
+
+        this_out_x = np.linspace(this_staring_x, this_ending_x, grid[1])
+        this_out_y = np.linspace(this_staring_y, this_ending_y, grid[1])
+
+        print(this_out_x)
+        print(this_out_y)
+
+        for j in range(this_out_x.shape[0]):
+            temp = [this_out_x[j],this_out_y[j]]
+            out.append(temp)
+            plt.plot(temp[0],temp[1],'ro')
+            plt.text(temp[0]+0.5,temp[1]+0.5, str(temp[0]) + '\n' + str(temp[1]))
+            plt.text(temp[0]-10,temp[1]-10, str(counter))
+            counter += 1
+    mng = plt.get_current_fig_manager()
+    mng.window.state('zoomed') #works fine on Windows!
+    plt.show()
+
+    print('eof')
